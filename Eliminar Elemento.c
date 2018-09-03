8a4345606c864943800bccaba1453ab7ff77e545
erisso3 <42158816+erisso3@users.noreply.github.com>
2018-09-02 23:37:41 -0500

    Add files via upload

---
+++ Eliminar Elemento.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Nodo{
+	int dato;
+	struct Nodo *siguiente;
+	struct Nodo *anterior;
+}Nodo;
+
+Nodo *inicio=NULL;
+Nodo *fin=NULL;
+
+void agregar(Nodo *nodo){
+	nodo -> siguiente=NULL;
+	if(inicio==NULL){
+		inicio=nodo;
+		fin=nodo;
+	}
+	else{
+		fin -> siguiente=nodo;
+		fin=nodo;
+	}
+}
+void eliminar_elemento(int elemento){
+	Nodo *aux=inicio;
+	Nodo *anterior=NULL;
+	while((aux!=NULL)&&(aux->dato!=elemento)){
+		anterior=aux;
+		aux=aux->siguiente;
+	}
+	if(aux!=NULL){
+		if(anterior==NULL){
+			inicio=aux->siguiente;
+		}
+		else{
+			anterior->siguiente=aux->siguiente;
+			aux->siguiente=NULL;
+			free(aux);
+		}
+	}
+}
+int main(int argc, char *argv[]) {
+	int elemento;
+	Nodo *primero=malloc(sizeof(Nodo));
+	primero -> dato = 2;
+	
+	Nodo *segundo=malloc(sizeof(Nodo));
+	segundo -> dato= 3;
+	
+	Nodo *tercero=malloc(sizeof(Nodo));
+	tercero -> dato=4;
+	
+	Nodo *cuarto=malloc(sizeof(Nodo));
+	cuarto -> dato=6;
+	
+	Nodo *quinto=malloc(sizeof(Nodo));
+	quinto -> dato=1;
+	
+	Nodo *sexto=malloc(sizeof(Nodo));
+	sexto -> dato=9;
+	
+	agregar(primero);
+	agregar(segundo);
+	agregar(tercero);
+	agregar(cuarto);
+	agregar(quinto);
+	agregar(sexto);
+	
+	Nodo *i=inicio;
+	
+	while(i!=NULL){
+		printf("%i\n",i->dato);
+		i = i -> siguiente;
+	}
+	
+	printf("\nSeleccione el elemento que desea remover:\n");
+	scanf("%d",&elemento);
+	eliminar_elemento(elemento);
+	i=inicio;
+	printf("\n\n");
+	while(i!=NULL){
+		printf("%i\n",i->dato);
+		i = i -> siguiente;
+	}
+	free(inicio);
+	free(fin);
+	free(primero);
+	free(segundo);
+	free(tercero);
+	free(cuarto);
+	free(quinto);
+	free(i);
+	return 0;
+}
+
---
+++ Listas enlazadas burbuja.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Nodo{
+	int dato;
+	struct Nodo *siguiente;
+	struct Nodo *anterior;
+}Nodo;//Aquí se definen los tipos de datos
+
+Nodo *inicio=NULL;
+Nodo *ultimo=NULL;//Se inicializan variables
+
+
+void agregar(Nodo *nodo){
+	nodo -> siguiente = NULL;
+	if(inicio==NULL){
+		inicio=nodo;
+		ultimo=nodo;
+	}
+	else{
+		ultimo -> siguiente = nodo;
+		ultimo = nodo;
+	}
+}//Esta función sirve para agregar datos
+
+void ordenar(){
+	Nodo *externo=inicio;
+	while(externo!=NULL){
+		Nodo *interno=externo->siguiente;
+		while(interno!=NULL){
+			if((externo->dato)<(interno->dato)){
+				int aux=externo->dato;
+				externo->dato=interno->dato;
+				interno->dato=aux;
+			}
+			interno=interno->siguiente;
+		}
+		externo=externo->siguiente;
+	}
+}//Esta función ordena los datos mediante burbuja
+int main(int argc, char *argv[]) {
+
+	
+	Nodo *primero=malloc(sizeof(Nodo));
+	primero -> dato = 2;
+	
+	Nodo *segundo=malloc(sizeof(Nodo));
+	segundo -> dato= 3;
+	
+	Nodo *tercero=malloc(sizeof(Nodo));
+	tercero -> dato=4;
+	
+	Nodo *cuarto=malloc(sizeof(Nodo));
+	cuarto -> dato=6;
+	
+	Nodo *quinto=malloc(sizeof(Nodo));
+	quinto -> dato=1;
+	
+	Nodo *sexto=malloc(sizeof(Nodo));
+	sexto -> dato=9;
+	
+	agregar(primero);
+	agregar(segundo);
+	agregar(tercero);
+	agregar(cuarto);
+	agregar(quinto);
+	agregar(sexto);
+	
+	Nodo *i=inicio;
+	
+	while(i!=NULL){
+		printf("%i\n",i->dato);
+		i = i -> siguiente;
+	}//Aquí se imprimen los datos tal como se ingresaron
+	ordenar();//Se llama la función de ordenar datos
+	
+	i=inicio;
+	printf("\n\n");
+	
+	while(i!=NULL){
+		printf("%i\n",i->dato);
+		i = i -> siguiente;
+	}//Aquí se imprimen los datos ya ordenados
+	return 0;
+}
+
